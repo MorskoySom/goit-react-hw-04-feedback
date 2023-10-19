a0b2d95180bd33b8b89358449531dcf418ba8912
MorskoySom <Somovvitvit@gmail.com>
2023-10-19 11:40:14 +0300

    fin1

--- src/App.js
+++ src/App.js
@@ -1,64 +1,62 @@
-import { Component } from "react";
+import { useState } from "react";
 import { Feedback } from "./FeedbackOptions/FeedbackOptions";
 import { Statistics } from "./Statistics/Statistics";
 import { Section } from "./Section/Section";
 import { Notification } from "./Notification/Notification"
 
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleClickUpdate = (type) => {
+    switch (type) {
+      case 'good':
+        return setGood(good + 1);
+      case 'neutral':
+        return setNeutral(neutral + 1);
+      case 'bad':
+        return setBad(bad + 1);
+    }
   }
 
-  handleClickUpdate = (type) => {
-    this.setState((prevState) => {
-      return {
-        [type]: prevState[type] + 1,
-      }
-    })
-  }
-
-  countTotalFeedback = () => {
-    const total = this.state.good + this.state.neutral + this.state.bad;
+  const countTotalFeedback = () => {
+    const total = good + neutral + bad;
     return total;
   }
 
-  countPositiveFeedbackPercentage = () => {
-    const { good, neutral, bad } = this.state;
+  const countPositiveFeedbackPercentage = () => {
     const percent = (good / (good + neutral + bad)) * 100;
     return percent > 0 ? ((good / (good + neutral + bad)) * 100).toFixed(2) : 0;
   }
 
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const options = Object.keys(this.state);
+  const options = ['good', 'neutral', 'bad'];
 
-    return (
-      <div>
-        <Section title="Please leave feedback">
-          <Feedback
-            options={options}
-            handleClickUpdate={this.handleClickUpdate} />
+  return (
+    <div>
+      <Section title="Please leave feedback">
+        <Feedback
+          options={options}
+          handleClickUpdate={handleClickUpdate} />
+      </Section>
+      {(good + neutral + bad) > 0 ? (
+        <Section title="Statistics">
+          <Statistics
+            countTotalFeedback={countTotalFeedback}
+            countPositiveFeedbackPercentage={countPositiveFeedbackPercentage}
+            good={good}
+            neutral={neutral}
+            bad={bad} />
         </Section>
-        {(good + neutral + bad) > 0 ? (
-          <Section title="Statistics">
-            <Statistics
-              countTotalFeedback={this.countTotalFeedback}
-              countPositiveFeedbackPercentage={this.countPositiveFeedbackPercentage}
-              good={good}
-              neutral={neutral}
-              bad={bad} />
-          </Section>
-        ) : (
-          <Section title="Statistics">
-            <Notification />
-          </Section>
-        )}
-      </div>
-    );
-  }
+      ) : (
+        <Section title="Statistics">
+          <Notification />
+        </Section>
+      )}
+    </div>
+  );
 }
+
 
 export default App;
